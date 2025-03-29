32105c79eea6bf02b89dd7f54bb7dac9b24d6a95
Taha <91777330+tahamukhtar20@users.noreply.github.com>
2025-03-29 18:03:07 +0500

    chore: Remove popup

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,10 @@ import { BentoGrid } from "@/components/ui/bento-grid"
 import { Testimonial } from "@/components/ui/testimonial"
 import { Highlights } from "@/components/ui/highlights"
 import { Sponsors } from "@/components/ui/sponsors"
-import Popup from "@/components/ui/popup"
 
 export default function Home() {
 	return (
 		<>
-			<Popup />
 			<Hero />
 			<BentoGrid />
 			<Testimonial />
